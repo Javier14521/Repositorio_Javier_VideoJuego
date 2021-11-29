89817becfed29b41bd272333375091e635a41b9b
Javier14521 <85377393+Javier14521@users.noreply.github.com>
2021-11-28 22:44:31 -0600

    Add files via upload

---
+++ Archivos/Pelota.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pelota : MonoBehaviour
+{
+    private Rigidbody rig;
+    [SerializeField] private float velocidad;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        rig.AddForce(new Vector3(horizontal, 0, 0) * velocidad *  Time.deltaTime);
+    }
+}
